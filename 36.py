cfd140d9c2a3c923fca23b06bd3b9920088cc2d7
jwy <lygjwy@qq.com>
2020-02-27 00:58:05 +0800

    error,debug&test

---
+++ 36.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'enum'
+
+__author__ = 'Yaptain Jiang'
+
+from enum import Enum, unique
+
+@unique
+class Gender(Enum):
+    Male = 0
+    Female = 1
+
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender
+
+if __name__ == '__main__':
+    # 测试:
+    bart = Student('Bart', Gender.Male)
+    if bart.gender == Gender.Male:
+        print('测试通过!')
+    else:
+        print('测试失败!')
